c90e44140cbc15d7cdf76567dded73ea4ce0f652
Yuki.kaco <a0936660023@gmail.com>
2022-06-11 20:18:04 +0800

    Upload Files

---
+++ dllmain.cpp
@@ -0,0 +1,76 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include "mem.h"
+#include <iostream>
+
+typedef BOOL(__stdcall* tCreateProcessA)(LPCSTR lpApplicationName,
+    LPSTR lpCommandLine,
+    LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    BOOL bInheritHandles,
+    DWORD dwCreationFlags,
+    LPVOID lpEnvironment,
+    LPCSTR lpCurrentDirectory,
+    LPSTARTUPINFOA lpStartupInfo,
+    LPPROCESS_INFORMATION lpProcessInformation);
+
+tCreateProcessA oCreateProcessA;
+
+BOOL __stdcall hCreateProcessA(LPCSTR lpApplicationName,
+    LPSTR lpCommandLine,
+    LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    BOOL bInheritHandles,
+    DWORD dwCreationFlags,
+    LPVOID lpEnvironment,
+    LPCSTR lpCurrentDirectory,
+    LPSTARTUPINFOA lpStartupInfo,
+    LPPROCESS_INFORMATION lpProcessInformation)
+{
+    std::string path = std::string(lpCommandLine);
+
+    path.append("-window");
+
+    std::cout << path << std::endl;
+
+    char* cstr = &path[0];
+
+    return oCreateProcessA(lpApplicationName,
+        cstr,
+        lpProcessAttributes,
+        lpThreadAttributes,
+        bInheritHandles,
+        dwCreationFlags,
+        lpEnvironment,
+        lpCurrentDirectory,
+        lpStartupInfo,
+        lpProcessInformation);
+}
+
+DWORD WINAPI HackThread(HMODULE hModule)
+{
+
+    oCreateProcessA = (tCreateProcessA)GetProcAddress(GetModuleHandle(L"KernelBase.dll"), "CreateProcessA");
+    oCreateProcessA = (tCreateProcessA)mem::TrampHook32((BYTE*)oCreateProcessA, (BYTE*)hCreateProcessA, 5);
+
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)HackThread, hModule, 0, 0);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ mem.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "pch.h"
+#include <Windows.h>
+#include <vector>
+
+namespace mem
+{
+	void PatchEx(BYTE* dst, BYTE* src, unsigned int size, HANDLE hProcess);
+	void NopEx(BYTE* dst, unsigned int size, HANDLE hProcess);
+	uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets);
+
+	void Patch(BYTE* dst, BYTE* src, unsigned int size);
+	void Nop(BYTE* dst, unsigned int size);
+	uintptr_t FindDMAAddy(uintptr_t ptr, std::vector<unsigned int> offsets);
+
+	bool Detour32(BYTE* src, BYTE* dst, const uintptr_t len);
+	BYTE* TrampHook32(BYTE* src, BYTE* dst, const uintptr_t len);
+}
